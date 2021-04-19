e436acca0ad5bb3019822245812e847c587e6073
Kevin Laborda <klaborda@gmail.com>
2021-04-18 20:49:38 -0600

    init graphql template

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { ApolloServer } = require("apollo-server");
+
+const fs = require("fs");
+const path = require("path");
+
+// 1
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+let idCount = links.length;
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
