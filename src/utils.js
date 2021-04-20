0c16e48b65dc94badb1ab83b86cfe563674d627f
Kevin Laborda <klaborda@gmail.com>
2021-04-19 22:53:47 -0600

    add helpers

---
+++ src/utils.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+// default to the env variable secret
+const APP_SECRET = process.env("APP_SECRET") || "sup3rs3cr3t";
+
+function getTokenPayload(token) {
+  return jwt.verify(token, APP_SECRET);
+}
+
+function getUserId(req, authToken) {
+  if (req) {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.replace("Bearer ", "");
+      if (!token) {
+        throw new Error("No token found");
+      }
+      const { userId } = getTokenPayload(token);
+      return userId;
+    }
+  } else if (authToken) {
+    const { userId } = getTokenPayload(authToken);
+    return userId;
+  }
+
+  throw new Error("Not authenticated");
+}
+
+module.exports = {
+  APP_SECRET,
+  getUserId,
+};
