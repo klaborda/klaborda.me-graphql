91a180cb0d0b77bbdf08e7b4da32f6740e09737d
Kevin Laborda <klaborda@gmail.com>
2021-04-19 22:51:46 -0600

    setting up user auth for API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
+const { getUserId } = require("./utils");
 const fs = require("fs");
 const path = require("path");
 
@@ -28,8 +29,12 @@ const prisma = new PrismaClient();
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
-  context: {
-    prisma,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
   },
 });
 
