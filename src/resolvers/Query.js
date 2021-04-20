a9f67e620b4364e1db03aafd71da72b83c9bf46d
Kevin Laborda <klaborda@gmail.com>
2021-04-20 10:09:09 -0600

    add filter support

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,17 @@
 function feed(parent, args, context, info) {
-  return context.prisma.post.findMany();
+  const where = args.filter
+    ? {
+        OR: [
+          { title: { contains: args.filter } },
+          { body: { contains: args.filter } },
+        ],
+      }
+    : {};
+
+  const posts = context.prisma.post.findMany({
+    where,
+  });
+  return posts;
 }
 
 module.exports = {
